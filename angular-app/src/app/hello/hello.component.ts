10bba69e291a1c7700fcdef53411740b561e2cd3
Vincent Letourneau <vincent@nanoninja.com>
2019-01-08 10:53:48 +0100

    Initial commit

---
+++ angular-app/src/app/hello/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-hello',
+    styleUrls: ['./hello.component.scss'],
+    templateUrl: './hello.component.html'
+})
+export class HelloComponent {
+    title = 'Le titre de mon composent';
+    
+}
